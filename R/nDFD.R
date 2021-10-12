a0451f2bdcb301438c44d0382501186f2a897b32
robertogattabs <roberto.gatta.bs@gmail.com>
2021-10-12 21:53:15 +0200

    .

---
+++ R/nDFD.R
@@ -0,0 +1,27 @@
+#' n Oder Direct Following Diagram Class
+#' 
+#' @description  a class for nDFD.
+#
+#' @import progress DiagrammeR
+#' @export
+nDFD<-function( parameters.list = list() ) {
+  
+  loadDataset<-function(  ) {
+    
+  }
+  getClass<-function(  ) {
+    
+  }
+  #=================================================================================
+  # costructor
+  #=================================================================================
+  costructor<-function( verboseMode ) {
+
+  }
+  costructor( verboseMode = verbose.mode);
+  #=================================================================================
+  return(list(
+    "loadDataset"=loadDataset,
+    "getClass"=getClass
+  ))
+}
